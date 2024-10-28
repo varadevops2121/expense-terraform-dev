6e346fc766f0645e8c23548b6f81846c2d521209
varalaxmi <varadevops2121@gmail.com>
2024-10-29 01:59:53 +0530

    expense-terraform-dev

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -18,3 +18,10 @@ resource "aws_ssm_parameter" "private_subnet_ids" {
   value = join(",",module.vpc.private_subnet_ids) # converting list to string list
 }
 
+resource "aws_ssm_parameter" "db_subnet_group_name" {
+  name  = "/${var.project_name}/${var.environment}/db_subnet_group_name"
+  type  = "String"
+  value = module.vpc.database_subnet_group_name
+}
+
+
